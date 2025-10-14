6bd4974ba73acd3a64baa4b4ce491af2326b3c16
nelviCastellanos <nelvicas97@gmail.com>
2025-10-14 14:59:45 -0600

    tipos de datos casing(conversion de tipos de datos

---
+++ temperatura/src/Main.java
@@ -0,0 +1,22 @@
+
+
+
+/*
+Escribe un programa que convierta una temperatura en grados Celsius a Fahrenheit.
+Utiliza variables para representar los valores de las temperaturas e imprime en la consola
+el valor convertido de Celsius a Fahrenheit.
+
+Consejo: La fórmula para convertir temperaturas de grados Celsius a Fahrenheit es:
+(temperatura * 1.8) + 32.
+Después de terminar, probar y verificar que tu programa se haya ejecutado correctamente,
+crea una variable entera para mostrar la temperatura en Fahrenheit sin decimales.
+recuerda que probablemente necesitarás realizar un casting de valores.
+
+
+ */
+
+
+
+public class Main {
+    public static void main(String[] args) {}
+}
