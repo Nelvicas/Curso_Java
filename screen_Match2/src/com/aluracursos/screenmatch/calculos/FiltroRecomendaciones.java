668c5d180afd0bf85c7bdedae21fab52fbab0780
nelviCastellanos <nelvicas97@gmail.com>
2025-10-31 21:24:27 -0600

    incluyendo comportamientos

--- screen_Match2/src/com/aluracursos/screenmatch/calculos/FiltroRecomendaciones.java
+++ screen_Match2/src/com/aluracursos/screenmatch/calculos/FiltroRecomendaciones.java
@@ -3,6 +3,7 @@ package com.aluracursos.screenmatch.calculos;
 public class FiltroRecomendaciones {
 
     public void filtra(Clasificacion clasificacion){
+
         if (clasificacion.getClasificacion() >= 4){
             System.out.println("Muy bien evaluado en el momento");
         }else if (clasificacion.getClasificacion() >= 2) {
