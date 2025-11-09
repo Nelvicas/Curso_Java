43af4cb009a2c34cfb71130769bd618365eed1fe
nelviCastellanos <nelvicas97@gmail.com>
2025-11-08 20:47:46 -0600

    proyecto final

---
+++ Proyecto/TarjetaDeCredito.java
@@ -0,0 +1,49 @@
+import Compra.Compra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TarjetaDeCredito {
+
+    /// /   variables privadas
+   private double limite;
+   private double saldo;
+   private List<Compra> listaDeCompras;
+
+
+
+   ///   Constructor
+    public TarjetaDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.listaDeCompras = new ArrayList<>();
+    }
+
+
+    ///  Metodo
+
+    public boolean lanzarCompra(Compra compra){
+        if(this.saldo >= compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.listaDeCompras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+
+
+    /// Getters
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getListaDeCompras() {
+        return listaDeCompras;
+    }
+}
